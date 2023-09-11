4016fb5a5b3c2f3e5a7c22fe5eab424ebb71504f
Franak007 <franck.pertosa@gmail.com>
2023-09-11 14:55:05 +0200

    optimisation code de chuck.component.ts

--- src/app/chuck/chuck.component.ts
+++ src/app/chuck/chuck.component.ts
@@ -1,15 +1,28 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {JokeInterface} from "../joke.interface";
+import {JokeService} from "../joke.service";
 
 @Component({
   selector: 'app-chuck',
   templateUrl: './chuck.component.html',
   styleUrls: ['./chuck.component.css']
 })
-export class ChuckComponent {
+export class ChuckComponent implements OnInit {
 
-  joke: JokeInterface;
+  joke: JokeInterface|undefined;
 
+  constructor(private jokeservice: JokeService) {
+  }
 
+  ngOnInit() {
+
+    this.jokeservice.getJoke().subscribe(jokeTranslate => {
+
+      this.joke = jokeTranslate;
+      console.log("Votre blague est égale à : " + this.joke.value)
+
+  });
+
+  }
 
 }
---
+++ src/app/joke.service.ts
@@ -0,0 +1,17 @@
+import {Injectable} from '@angular/core';
+import {HttpClient} from "@angular/common/http";
+import {Observable} from "rxjs";
+import {JokeInterface} from "./joke.interface";
+
+@Injectable({
+  providedIn: 'root'
+})
+export class JokeService {
+
+  constructor(private http: HttpClient) { }
+
+  getJoke(): Observable<JokeInterface>{
+    return this.http.get<JokeInterface>('https://api.chucknorris.io/jokes/random');
+  }
+
+}
