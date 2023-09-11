9c99fd056109884dc7762dc7687945219e81b655
Franak007 <franck.pertosa@gmail.com>
2023-09-11 11:07:04 +0200

    création navbar puis insertion sur chaque page de component (conseillé)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {HomeComponent} from "./home/home.component";
+import {SecondPageComponent} from "./second-page/second-page.component";
 
 const routes: Routes = [
-  {path: '', component: HomeComponent}
+  {path: '', component: HomeComponent},
+  {path: 'second', component: SecondPageComponent}
 ];
 
 @NgModule({
