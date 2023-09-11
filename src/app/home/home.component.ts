ecc9b1c6043b2e5bbf309cf7d0fddf7d9cd2489f
Franak007 <franck.pertosa@gmail.com>
2023-09-11 10:07:48 +0200

    erreur avec app.component.ts, OnInit ajouté dans home.component.ts

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,10 +1,15 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.css']
 })
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+  titleHome: string = "Chuck Norris"
+
+  ngOnInit() {
+    console.log("Le composant vient d'être chargé")
+  }
 
 }
