f2aad709ced972cd887a5b1209bdb286f39e4fb8
Franak007 <franck.pertosa@gmail.com>
2023-09-11 12:03:05 +0200

    création component chuck norris pour exploiter données API

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,21 +1,31 @@
+//Fonctionnalités
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import {HttpClientModule} from "@angular/common/http";
 
+//Composants et modules
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { SecondPageComponent } from './second-page/second-page.component';
 import { NavbarComponent } from './navbar/navbar.component';
+import { ChuckComponent } from './chuck/chuck.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HomeComponent,
     SecondPageComponent,
-    NavbarComponent
+    NavbarComponent,
+    ChuckComponent
   ],
   imports: [
+
+    //Fonctionnalités
     BrowserModule,
+    HttpClientModule,
+
+    //Composants et modules
     AppRoutingModule
   ],
   providers: [],
---
+++ src/app/chuck/chuck.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-chuck',
+  templateUrl: './chuck.component.html',
+  styleUrls: ['./chuck.component.css']
+})
+export class ChuckComponent {
+
+  joke: string = "";
+
+}
